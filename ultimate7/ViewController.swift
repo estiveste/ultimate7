4897890d7ff166840a04e4872e5c0370673f785c
Jose Esteves <estevesdeveloper@gmail.com>
2017-01-03 21:23:52 -0200

    Initial Commit

---
+++ ultimate7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ultimate7
+//
+//  Created by José Esteves on 03/01/17.
+//  Copyright © 2017 José Esteves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ultimate7Tests/ultimate7Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ultimate7Tests.swift
+//  ultimate7Tests
+//
+//  Created by José Esteves on 03/01/17.
+//  Copyright © 2017 José Esteves. All rights reserved.
+//
+
+import XCTest
+@testable import ultimate7
+
+class ultimate7Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
